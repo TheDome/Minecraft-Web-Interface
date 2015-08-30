00af380b00a790a572c12bad63d59197968c6a22
TheDome <TheDome@users.noreply.github.com>
2015-08-30 19:20:01 +0200

    Change Username and Password

--- script.sh
+++ script.sh
@@ -10,8 +10,8 @@ sudo apt-get -y install apache2
  
 # Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
  
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password vagrant'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password vagrant'
 sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
  
 # Installing PHP and it's dependencies
